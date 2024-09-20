4f806ba668d17e2ffea5d5cb3e6ff34fe5e6b71a
Aries Wibowo <riespratama@gmail.com>
2024-09-20 17:02:09 +0700

    Tambah Model Tamu

--- RegistrationApp/models.py
+++ RegistrationApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Meja(model):
+    nomor_meja = models.IntegerField()
+
+class Tamu(models.Model):
+    instansi = models.CharField(max_length=128)
+    nama = models.CharField(max_length=128)
+    meja = models.ForeignKey(Meja, on_delete=models.CASCADE)
