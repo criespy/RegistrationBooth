7494ad4273f5d7d37a06bfb456f1918ca8b820cf
Aries Wibowo <riespratama@gmail.com>
2024-09-23 15:16:48 +0700

    Tambahan di Model: Improve Tamu, Add CheckIn

--- RegistrationApp/models.py
+++ RegistrationApp/models.py
@@ -3,6 +3,17 @@ import qrcode
 from io import BytesIO
 from django.core.files import File
 from PIL import Image, ImageDraw
+import random
+import os
+from django.conf import settings
+
+def generate_random_number():
+        return random.randrange(10000000, 100000000)
+
+def get_qr_code_upload_path(instance, filename):
+    # This will create a path: 'qr_codes/qr_code_<name>.png'
+    return os.path.join('qr_codes', f'qr_code_{instance.nama}.png')
+
 
 class Meja(models.Model):
     nomor_meja = models.IntegerField()
@@ -16,9 +27,10 @@ class Meja(models.Model):
 
 class Tamu(models.Model):
     instansi = models.CharField(max_length=128)
-    nama = models.CharField(max_length=128)
+    nama = models.CharField(max_length=128, null=True, blank=True)
     meja = models.ForeignKey(Meja, on_delete=models.CASCADE)
-    qr_code = models.ImageField(upload_to='qr_codes/', blank=True)
+    qr_code = models.ImageField(upload_to=get_qr_code_upload_path, blank=True)
+    rand_code = models.CharField(max_length=8, unique=True, blank=True)
 
     class Meta:
         verbose_name = 'Tamu'
@@ -28,17 +40,30 @@ class Tamu(models.Model):
         return f"{self.nama} {self.instansi}"
     
     def save(self, *args, **kwargs):
+        # Check if an old QR code exists and delete it
+        if self.qr_code:
+            file_path = os.path.join(settings.MEDIA_ROOT, self.qr_code.name)
+            if os.path.exists(file_path):
+                os.remove(file_path)
+
         #Buat QR Code nya
-        qr_image = qrcode.make(self.url)
-        canvas = Image.new('RGB', (qr_image.pixel_size), 'white')
+        qr_data = f"FLN_E01_{self.id}"
+        qr_image = qrcode.make(self.rand_code)
+        canvas = Image.new('RGB', (qr_image.size), 'white')
         draw = ImageDraw.Draw(canvas)
         canvas.paste(qr_image)
+        if not self.rand_code:
+            self.rand_code = generate_random_number()
 
         #Save ke memory
         buffer = BytesIO()
         canvas.save(buffer, format='PNG')
-        file_name = f'qr_code_{self.nama}.png'
+        file_name = f'qr_code_{self.instansi}_{self.nama}.png'
 
         #Save ke DB
-        self.qr_code.save(file_name, File(bufer), save=False)
+        self.qr_code.save(file_name, File(buffer), save=False)
         super().save(*args, **kwargs)
+
+class CheckIn(models.Model):
+    waktu = models.DateTimeField(auto_now_add=True)
+    tamu = models.ForeignKey(Tamu, on_delete=models.CASCADE)
